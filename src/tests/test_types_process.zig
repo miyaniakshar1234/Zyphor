8ed57753fe22330bc912c7cea29b1a8253582353
Aksharbhai Miyani <miyaniakshar1234@gmail.com>
2026-09-03 23:39:26 +0530

    test(types): add unit tests for ProcessInfo name trimming and fields

---
+++ src/tests/test_types_process.zig
@@ -0,0 +1,18 @@
+const std = @import("std");
+const types = @import("../core/types.zig");
+
+test "ProcessInfo name trimming and metrics" {
+    var proc = types.ProcessInfo{
+        .pid = 1337,
+        .ppid = 1,
+        .cpu_percent = 12.5,
+        .memory_rss_bytes = 64 * 1024 * 1024,
+    };
+    const name = "zyphor.exe";
+    @memcpy(proc.name[0..name.len], name);
+    proc.name_len = name.len;
+
+    try std.testing.expectEqualStrings(name, proc.getName());
+    try std.testing.expectEqual(@as(u32, 1337), proc.pid);
+    try std.testing.expectEqual(@as(f32, 12.5), proc.cpu_percent);
+}
